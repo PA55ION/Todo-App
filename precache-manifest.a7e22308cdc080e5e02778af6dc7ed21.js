68d39b85df5e1318964cfdbc0b18e0278cc06efc
Mai Campbell <jirallc@gmail.com>
2019-12-08 22:15:41 -0800

    Updates

---
+++ precache-manifest.a7e22308cdc080e5e02778af6dc7ed21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Todo-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "80f4e8aad60d05c6b8be",
+    "url": "/Todo-App/static/css/main.23051b95.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Todo-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "021e4d3bc407b68201ab",
+    "url": "/Todo-App/static/js/1.021e4d3b.chunk.js"
+  },
+  {
+    "revision": "58db14a1c0e0e44501c3",
+    "url": "/Todo-App/static/js/runtime~main.58db14a1.js"
+  },
+  {
+    "revision": "80f4e8aad60d05c6b8be",
+    "url": "/Todo-App/static/js/main.80f4e8aa.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Todo-App/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Todo-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Todo-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "021e4d3bc407b68201ab",
+    "url": "/Todo-App/static/css/1.29dcca57.chunk.css"
+  },
+  {
+    "revision": "8833af79a2a3894db24f6afc5f0af128",
+    "url": "/Todo-App/index.html"
+  }
+];
